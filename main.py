495728410f28262fb1c0d7f819545068768b75c2
Michele <mscomina@gmail.com>
2024-05-26 01:41:32 +0200

    First commit.

---
+++ main.py
@@ -0,0 +1,10 @@
+#
+#   Code for the "Introduction to Machine Learning and Evolutonary Robotics" project.
+#
+import train
+
+def main():
+    train.train_model()
+
+if __name__ == "__main__":
+    main()
